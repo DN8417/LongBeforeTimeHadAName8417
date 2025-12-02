911c4c6cd5305bebb93febedf309bd20132502b0
DN8417 <dgnolan09@gmail.com>
2025-12-02 16:30:12 -0500

    Even more auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/init/RedSideBottomAuto.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.init;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Red Bottom", group ="Autos")
+public class RedSideBottomAuto extends LinearOpMode {
+
+    public DcMotor frontRightDrive;
+    public DcMotor frontLeftDrive;
+    public DcMotor backRightDrive;
+    public DcMotor backLeftDrive;
+
+
+    public ElapsedTime timer = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws  InterruptedException {
+
+        frontRightDrive = hardwareMap.get(DcMotor.class, "Front Right");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "Front Left");
+        backRightDrive = hardwareMap.get(DcMotor.class, "Back Right");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "Back Left");
+
+        frontLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        // Forward
+        mecanumDrive(0.75, 0.75, 0.75, 0.75, 0.5);
+        // Moves left
+        mecanumDrive(0.75, -0.75, -0.75, 0.75, 0.5);
+
+
+    }
+
+    public void mecanumDrive(double frontRightPower, double frontLeftPower, double backRightPower, double backLeftPower, double seconds) {
+
+        timer.reset();
+
+        while(opModeIsActive() && (timer.seconds() <= seconds )) {
+
+            frontRightDrive.setPower(frontRightPower);
+            frontLeftDrive.setPower(frontLeftPower);
+            backRightDrive.setPower(backRightPower);
+            backLeftDrive.setPower(backLeftPower);
+        }
+
+    }
+}
