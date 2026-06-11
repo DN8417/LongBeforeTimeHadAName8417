48cd6b5ad157874dcad15047ba6f32f013299115
DN8417 <dgnolan09@gmail.com>
2026-06-11 11:50:20 -0400

    Update RedSideFarAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/init/RedSideFarAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/init/RedSideFarAuto.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.init;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -12,6 +15,10 @@ import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.action.FlyWheel;
 import org.firstinspires.ftc.teamcode.action.touchSensor;
 
 @Autonomous(name="Red Far Zone", group ="Autos")
@@ -26,12 +33,20 @@ public class RedSideFarAuto extends LinearOpMode {
     public CRServo smallWheel;
     public CRServo intakePartTwo;
     public DcMotor intakeMotor;
+    FlyWheel flyWheel = new FlyWheel();
     private IMU imu;
+    private double encoderCPM = 28;
+    private double gearRatio = 3 / 2;
+    private double kV = 0.000171, kS = 0.09, kP = 0.0009;
+    public double lowVelocity = 3250;
+    public double highVelocity = 4500;
+    private double targetRpm = highVelocity;
+    Telemetry telemetry;
+    boolean lastX = false;
     private double distance;
     private Limelight3A limelight;
     touchSensor touchsensor = new touchSensor();
     private CRServo turretMotor;
-
 
     public ElapsedTime timer = new ElapsedTime();
 
@@ -44,7 +59,7 @@ public class RedSideFarAuto extends LinearOpMode {
         backLeftDrive = hardwareMap.get(DcMotor.class, "Back Left");
         rubberBandWheel = hardwareMap.get(DcMotor.class, "Rubber Band Wheel");
         m1 = hardwareMap.get(DcMotorEx.class, "Turret Launcher");
-        //m2 = hardwareMap.get(DcMotorEx.class, "Turret Launcher 2");
+        m2 = hardwareMap.get(DcMotorEx.class, "Turret Launcher 2");
         smallWheel = hardwareMap.get(CRServo.class, "Small Wheel");
         intakeMotor = hardwareMap.get(DcMotor.class, "Intake");
         intakePartTwo = hardwareMap.get(CRServo.class, "Second Intake");
@@ -62,7 +77,7 @@ public class RedSideFarAuto extends LinearOpMode {
         limelight.pipelineSwitch(0); // april tag #11 pipeline
 
         imu= hardwareMap.get(IMU.class, "imu");
-        RevHubOrientationOnRobot revHubOrientationOnRobot= new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+        RevHubOrientationOnRobot revHubOrientationOnRobot= new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
                 RevHubOrientationOnRobot.UsbFacingDirection.UP);
         imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
 
@@ -97,9 +112,9 @@ public class RedSideFarAuto extends LinearOpMode {
         // Wait for a moment
         mecanumDrive(0.00, 0.00, 0.00, 0.00, 0.00, -200, 0.00, 1.00, -0.5, 1.0);
         // Moves Backwards while intaking
-        mecanumDrive(-0.75, -0.75, -0.75, -0.75, 0.00, -200, 0.00, 1.00, -0.5, 0.7);
+        mecanumDrive(-0.3, -0.3, -0.3, -0.3, 0.00, -200, 0.00, 1.00, -0.5, 2.2);
         // Moving artifacts along the intake
-        mecanumDrive(0.00, 0.00, 0.00, 0.00, 0.00, -200, 1.00, 1.00, -0.5, 1.8);
+        mecanumDrive(0.00, 0.00, 0.00, 0.00, 0.00, -200, 1.00, 1.00, -0.5, 2.3);
         // Moving back into position
         mecanumDrive(0.75, 0.75, 0.75, 0.75, 0.00, -200, 0.00, 0.00, 0.00, 0.7);
         mecanumDrive(-0.75, 0.75, 0.75, -0.75, 0.00, -200, 0.00, 0.00, 0.00, 0.2);
@@ -140,40 +155,48 @@ public class RedSideFarAuto extends LinearOpMode {
             backRightDrive.setPower(backRightPower);
             backLeftDrive.setPower(backLeftPower);
             rubberBandWheel.setPower(rubberBandPower);
-            m1.setVelocity(launcherVelocity);
+            targetRpm = highVelocity;
+            double getTicksPerSec = m1.getVelocity();
+            double getRPM = ((getTicksPerSec / encoderCPM) * 60) / gearRatio;
+            double error = targetRpm - getRPM;
+            double ff = (kV * targetRpm) + kS;
+            double fb = error * kP;
+            double power = ff + fb;
+            m1.setPower(power);
+            m2.setPower(power);
+            //m1.setVelocity(launcherVelocity);
             //m2.setVelocity(launcherVelocity);
             smallWheel.setPower(smallWheelPower);
             intakePartTwo.setPower(secondIntakePower);
             intakeMotor.setPower(intakePower);
 
-//            YawPitchRollAngles orientation= imu.getRobotYawPitchRollAngles();
-//            limelight.updateRobotOrientation(orientation.getYaw());
-//            LLResult llResult = limelight.getLatestResult();
-//            if (llResult != null && llResult.isValid()) {
-//                Pose3D botpose = llResult.getBotpose_MT2();
-//                //distance = getDistanceFromTage(llResult.getTa());
+            YawPitchRollAngles orientation= imu.getRobotYawPitchRollAngles();
+            limelight.updateRobotOrientation(orientation.getYaw());
+            LLResult llResult = limelight.getLatestResult();
+            if (llResult != null && llResult.isValid()) {
+                Pose3D botpose = llResult.getBotpose_MT2();
+                //distance = getDistanceFromTage(llResult.getTa());
 //                telemetry.addData("distance", distance);
 //                telemetry.addData("Tx", llResult.getTx());
 //                telemetry.addData("Ta", llResult.getTa());
-//
-//            }
-//            double Tx = llResult.getTx();
-//
-//            if (Tx < -3 && !touchsensor.leftTouchSensorIsPressed()) {
-//                telemetry.addData("Tx", "TurretLeft");
-//                turretMotor.setPower(-0.2);
-//            }
-//
-//            else if (Tx > 3 && !touchsensor.leftTouchSensorIsPressed()) {
-//                telemetry.addData("Tx", "TurretRight");
-//                turretMotor.setPower(0.2);
-//            }
-//
-//            else {
-//                telemetry.addData("Tx", "Good");
-//                turretMotor.setPower(0);
-//            }
 
+            }
+            double Tx = llResult.getTx();
+
+            if (Tx < -3 && !touchsensor.leftTouchSensorIsPressed()) {
+                //telemetry.addData("Tx", "TurretLeft");
+                turretMotor.setPower(-0.2);
+            }
+
+            else if (Tx > 3 && !touchsensor.leftTouchSensorIsPressed()) {
+                //telemetry.addData("Tx", "TurretRight");
+                turretMotor.setPower(0.2);
+            }
+
+            else {
+                //telemetry.addData("Tx", "Good");
+                turretMotor.setPower(0);
+            }
         }
 
     }
